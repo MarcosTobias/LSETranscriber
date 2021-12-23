9e7069be478742f4306ef9c2887704a6f59ca3b6
UO270930 <marcostobias64@gmail.com>
2021-12-23 13:47:39 +0100

    Dockerfile for the restapi

--- restapi/makePrediction.py
+++ restapi/makePrediction.py
@@ -5,7 +5,7 @@ import cv2
 
 class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U']
 
-model = tf.keras.models.load_model("model\\trained_tuned_ext2")
+model = tf.keras.models.load_model("model/trained_tuned_ext2")
 
 probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
